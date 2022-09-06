14547579e16994b662609ad3fb17f5ffad937147
Maxime Dev <maximedaude.dev@gmail.com>
2022-09-06 17:47:35 +0200

    add comment

--- boardHandler.go
+++ boardHandler.go
@@ -5,6 +5,14 @@ import (
 	"net/http"
 )
 
+// GET /board:
+// Devra renvoyer l’état du plateau. Un exemple de format possible peut être plusieurs suites
+// de chiffres:
+// - 0: case intouchée, non dévoilée
+// - 1: coup dans l’eau
+// - 2: bateau touché
+// Libre à vous de choisir un meilleur format de communication.
+
 func BoardHandler(board Board) func(w http.ResponseWriter, req *http.Request) {
 	boardHandlerReturn := func(w http.ResponseWriter, req *http.Request) {
 		fmt.Println(board)

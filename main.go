8b5d715ba0de58889fd824b7ed25df63d6cccb5f
Maxime Dev <maximedaude.dev@gmail.com>
2022-09-07 09:32:41 +0200

    add /boats

--- main.go
+++ main.go
@@ -9,39 +9,34 @@ import (
 
 var board Board
 
-func boatHandler(w http.ResponseWriter, req *http.Request) {
-}
-
 func hit(x, y int) func(w http.ResponseWriter, req *http.Request) {
 	hitHandler := func(w http.ResponseWriter, req *http.Request) {
-	switch req.Method {
+		switch req.Method {
 		case http.MethodGet:
-		fmt.Fprintf(w, "you can attack using a post request with x and y coordinates");
+			fmt.Fprintf(w, "you can attack using a post request with x and y coordinates")
 		case http.MethodPost:
 			// get coordinates
 			var coors Coors
-			x, err :=  strconv.Atoi(req.FormValue("x"))
-			y, err :=  strconv.Atoi(req.FormValue("y"))
+			x, err := strconv.Atoi(req.FormValue("x"))
+			y, err := strconv.Atoi(req.FormValue("y"))
 			if err != nil {
 				log.Fatal(err)
 			}
 			coors.x = x
 			coors.y = y
-			board = hitBoard(board,coors);
-			fmt.Fprintf(w, "you hit the board at %d,%d", x, y);
+			board = hitBoard(board, coors)
+			fmt.Fprintf(w, "you hit the board at %d,%d", x, y)
+		}
 	}
+	return hitHandler
 }
-return hitHandler
-}
-
-
 
 func main() {
 	board = GenerateBoard(10)
 	fmt.Println("Starting web server at 0.0.0.0:3001...")
 	http.HandleFunc("/board", boardHandler())
-	http.HandleFunc("/boats", boatHandler)
-	http.HandleFunc("/hit", hit(1,5))
+	http.HandleFunc("/boats", boatsHandler(board))
+	http.HandleFunc("/hit", hit(1, 5))
 	go play(board)
 	if err := http.ListenAndServe("0.0.0.0:3001", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)

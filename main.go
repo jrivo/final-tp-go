1c5bf7263ccc9804ed679ef17cd79529b129e3a0
Jonathan Rivo <rivo.jonathan98@gmail.com>
2022-09-07 12:07:03 +0200

    Fixed conflicts

--- main.go
+++ main.go
@@ -5,36 +5,34 @@ import (
 	"log"
 	"main/battleship/server"
 	"net/http"
-	"strconv"
-	"runtime"
 	"os/exec"
+	"runtime"
+	"strconv"
 )
 
-
-func hit(board *Board,x int , y int) func(w http.ResponseWriter, req *http.Request) {
+func hit(board *Board, x int, y int) func(w http.ResponseWriter, req *http.Request) {
 	hitHandler := func(w http.ResponseWriter, req *http.Request) {
-	switch req.Method {
+		switch req.Method {
 		case http.MethodGet:
-		fmt.Fprintf(w, "you can attack using a post request with x and y coordinates");
+			fmt.Fprintf(w, "you can attack using a post request with x and y coordinates")
 		case http.MethodPost:
 			// get coordinates
 			var coors Coors
-			x, err :=  strconv.Atoi(req.FormValue("x"))
-			y, err :=  strconv.Atoi(req.FormValue("y"))
+			x, err := strconv.Atoi(req.FormValue("x"))
+			y, err := strconv.Atoi(req.FormValue("y"))
 			if err != nil {
 				log.Fatal(err)
 			}
 			coors.x = x
 			coors.y = y
-			*board = hitBoard(board,coors);
-			fmt.Fprintf(w, "you hit the board at %d,%d", x, y);
+			*board = hitBoard(board, coors)
+			fmt.Fprintf(w, "you hit the board at %d,%d", x, y)
 			// print the board
 			fmt.Println(board)
+		}
 	}
+	return hitHandler
 }
-return hitHandler
-}
-
 
 func openbrowser(url string) {
 	var err error
@@ -62,7 +60,7 @@ func main() {
 	go http.HandleFunc("/board", boardHandler(&board))
 	go http.HandleFunc("/boats", boatsHandler(board))
 	// send board reference in hit function
-	go http.HandleFunc("/hit", hit(&board,1,5))
+	go http.HandleFunc("/hit", hit(&board, 1, 5))
 	myChannel1 := make(chan bool)
 	go play(board, myChannel1)
 	openbrowser("http://localhost:3001/board")
